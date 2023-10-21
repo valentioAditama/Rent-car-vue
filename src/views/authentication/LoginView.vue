72045872bf13905c74315786ba5e190e3a6c38e7
valentioaditama <valentioaditama8@gmail.com>
2023-10-21 23:05:58 +0700

    update auth

--- src/views/authentication/LoginView.vue
+++ src/views/authentication/LoginView.vue
@@ -1,71 +1,57 @@
 <template>
-  <div class="grid grid-cols-3 gap-4">
-    <div
-      class="h-screen bg-cover bg-center col-span-2"
-      style="
-        background-image: url('https://images.unsplash.com/photo-1557825631-19082bca3803?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1495&q=80');
-      "
-    ></div>
-    <div class="flex h-screen">
-      <div class="grid grid-cols-1 gap-4 content-center m-auto">
-        <div class="leading-5 mb-2">
-          <p class="text-4xl">Welcome Back!</p>
-          <small>Please Login First if u want to Rent</small>
-        </div>
-        <form action="" method="post">
-          <div class="mb-3">
-            <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Email</label>
-            <input
-              type="text"
-              id="email"
-              name="email"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="email@gmail.com"
-              required
-            />
-          </div>
-          <div class="mb-3">
-            <label for="password" class="block mb-2 text-sm font-medium text-gray-900"
-              >Password</label
-            >
-            <input
-              type="password"
-              id="password"
-              name="password"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="password"
-              required
-            />
-          </div>
-          <div class="flex items-center mb-3">
-            <input
-              id="default-checkbox"
-              type="checkbox"
-              value=""
-              class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-            />
-            <label for="default-checkbox" class="ml-2 text-sm font-medium dark:text-black"
-              >Show Password</label
-            >
-          </div>
-          <div class="mb-3">
-            <button
-              type="submit"
-              class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Login
-            </button>
-            <a href="/register">
-              <button
-                type="button"
-                class="w-full py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-              >
-                Didn't Have Account?
-              </button>
-            </a>
-          </div>
-        </form>
+  <!-- component -->
+<section class="flex flex-col md:flex-row h-screen items-center">
+
+<div class="bg-indigo-600 hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen">
+  <img src="https://source.unsplash.com/random" alt="" class="w-full h-full object-cover">
+</div>
+
+<div class="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12
+      flex items-center justify-center">
+
+  <div class="w-full h-100">
+
+
+    <h1 class="text-xl md:text-2xl font-bold leading-tight mt-12">Log in to your account</h1>
+
+    <form class="mt-6" action="#" method="POST">
+      <div>
+        <label class="block text-gray-700">Email Address</label>
+        <input type="email" name="" id="" placeholder="Enter Email Address" class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autofocus autocomplete required>
       </div>
-    </div>
+
+      <div class="mt-4">
+        <label class="block text-gray-700">Password</label>
+        <input type="password" name="" id="" placeholder="Enter Password" minlength="6" class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500
+              focus:bg-white focus:outline-none" required>
+      </div>
+
+      <div class="text-right mt-2">
+        <a href="#" class="text-sm font-semibold text-gray-700 hover:text-blue-700 focus:text-blue-700">Forgot Password?</a>
+      </div>
+
+      <button type="submit" class="w-full block bg-indigo-500 hover:bg-indigo-400 focus:bg-indigo-400 text-white font-semibold rounded-lg
+            px-4 py-3 mt-6">Log In</button>
+    </form>
+
+    <hr class="my-6 border-gray-300 w-full">
+
+    <button type="button" class="w-full block bg-white hover:bg-gray-100 focus:bg-gray-100 text-gray-900 font-semibold rounded-lg px-4 py-3 border border-gray-300">
+          <div class="flex items-center justify-center">
+          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="w-6 h-6" viewBox="0 0 48 48"><defs><path id="a" d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z"/></defs><clipPath id="b"><use xlink:href="#a" overflow="visible"/></clipPath><path clip-path="url(#b)" fill="#FBBC05" d="M0 37V11l17 13z"/><path clip-path="url(#b)" fill="#EA4335" d="M0 11l17 13 7-6.1L48 14V0H0z"/><path clip-path="url(#b)" fill="#34A853" d="M0 37l30-23 7.9 1L48 0v48H0z"/><path clip-path="url(#b)" fill="#4285F4" d="M48 48L17 24l-4-3 35-10z"/></svg>
+          <span class="ml-4">
+          Log in
+          with
+          Google</span>
+          </div>
+        </button>
+
+    <p class="mt-8">Need an account? <a href="/register" class="text-blue-500 hover:text-blue-700 font-semibold">Create an
+            account</a></p>
+
+
   </div>
+</div>
+
+</section>
 </template>
